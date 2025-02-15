c08542dc797ac8243acbfb968ddcfdfa56fe2481
RC-Mahjong <160792288+RC-Mahjong@users.noreply.github.com>
2025-02-16 00:51:08 +0100

    Update language_swapper.js

--- language_swapper.js
+++ language_swapper.js
@@ -2,7 +2,7 @@ const languageData = {
     "en": {
       "about_us": "About Us",
       "language": "Language",
-      "current_language": `<span class="flag-icon flag-icon-us"></span>EN`,
+      "current_language": `<span class="flag-icon flag-icon-us"></span> EN`,
       "pdfs": "PDFs",
       "venue": "Gaming Venue",
       "links": "Links",
@@ -43,7 +43,7 @@ const languageData = {
     "de": {
       "about_us": "Über Uns",
       "language": "Sprache",
-      "current_language": `<span class="flag-icon flag-icon-de"></span>DE`,
+      "current_language": `<span class="flag-icon flag-icon-de"></span> DE`,
       "pdfs": "PDFs",
       "venue": "Spielort",
       "links": "Links",
